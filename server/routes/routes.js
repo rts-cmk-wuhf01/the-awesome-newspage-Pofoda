b3a2814f6635fc5a4d9c0d3620be3462440c4d7a
Pofoda <3011904@rts-365.dk>
2019-05-29 10:36:07 +0200

    commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,5 +3,21 @@ module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home');
    });
+   
+   app.get('/catagories', (req, res, next) => {
+      res.render('cat-post');
+   });
+   
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
+   
+   app.get('/about', (req, res, next) => {
+      res.render('about');
+   });
+   
+   app.get('/single', (req, res, next) => {
+      res.render('single-post');
+   });
 
 };
